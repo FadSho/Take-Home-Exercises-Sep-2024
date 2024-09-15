bee812ec5a7b15808eb66227b7f398847ca36781
Dinkyfaddy <fadekeshodeinde@yahoo.com>
2024-09-15 04:53:34 -0600

    Create Class Author
    Confirmation of commit is functioning

---
+++ BookSystem/BookSystemLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace BookSystemLibrary
+{
+    public class Author
+    {
+        #region Data Members
+
+        private string _FirstName;
+        private string _LastName;
+        private string _ContactUrl;
+        private string _ResidentCity;
+        private string _ResidentCountry;
+        #endregion
+
+    }
+}
